e38a26e4ea823a821ca922b57a75b52bf418dba0
GrajaKonrad <konrad.graja@gmail.com>
2022-10-27 17:42:48 +0200

    zmiana3

---
+++ 04-git/Program.java
@@ -0,0 +1,19 @@
+package com.w3spoint;
+ 
+import java.util.Random;
+ 
+public class Test {
+	public static void main(String args[]){
+		Random random = new Random();		
+		//It generates boolean value
+		System.out.println(random.nextBoolean());		
+		//It generates double value
+		System.out.println(random.nextDouble());		
+		//It generates float value
+		System.out.println(random.nextFloat());		
+		//It generates int value
+		System.out.println(random.nextInt());		
+		//It generates int value within specific limit
+		System.out.println(random.nextInt(50));
+	}
+}
